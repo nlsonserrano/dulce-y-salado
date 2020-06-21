34865aa6cefe83eabc2f29191897d4f380bbd630
nlsonserrano <66968144+nlsonserrano@users.noreply.github.com>
2020-06-21 23:27:15 +0200

    Add files via upload

---
+++ dulceysalado/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.movil').addEventListener('click', () => {
+    document.querySelector('.menu').classList.toggle('show');
+});
+
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelector('.menu').classList.toggle('show');
+});
